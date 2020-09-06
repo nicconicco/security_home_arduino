7d3767d4219834541be294497724840b04a45e67
Carlos N Galves <nicolaugalves@gmail.com>
2020-09-06 19:47:27 -0300

    Keyboard works

--- security_home_arduino/security_home_arduino.ino
+++ security_home_arduino/security_home_arduino.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Keypad.h>
 
+int led_pin=13;
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+void setup(){
+  pinMode(led_pin,OUTPUT);
+  Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+void loop(){
+  char customKey = customKeypad.getKey();
+  
+  if (customKey){
+    digitalWrite(led_pin,HIGH);
+    Serial.println(customKey);
+    delay(100);
+  }
+   else digitalWrite(led_pin,LOW);
 }
